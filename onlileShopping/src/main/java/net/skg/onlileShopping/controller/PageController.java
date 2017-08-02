d8f24667b5ef563b3d5d4292f0ddfba39fc63160
sourav <sourav@gayen-PC>
2017-08-02 23:21:42 +0530

    After creating the template

---
+++ onlileShopping/src/main/java/net/skg/onlileShopping/controller/PageController.java
@@ -0,0 +1,38 @@
+package net.skg.onlileShopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value={"/", "/home", "/index"})
+	public ModelAndView index(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting","Welcome to Spring MVC");
+		return mv;
+	}
+//	@RequestMapping(value="test")
+//	public ModelAndView test(@RequestParam (value="greeting", required=false) String greeting){
+//		ModelAndView mv=new ModelAndView("page");
+//		if(null == greeting)
+//			mv.addObject("greeting","Hello there");
+//		else
+//			mv.addObject("greeting",greeting);
+//		return mv;
+//	}
+	
+	@RequestMapping(value="test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting") String greeting){
+		ModelAndView mv=new ModelAndView("page");
+		if(null == greeting)
+			mv.addObject("greeting","Hello there");
+		else
+			mv.addObject("greeting",greeting);
+		
+		System.out.println(greeting);
+		return mv;
+	}
+}
---
+++ onlileShopping/src/main/java/net/skg/onlileShopping/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author maiti
+ *
+ */
+package net.skg.onlileShopping.controller;
