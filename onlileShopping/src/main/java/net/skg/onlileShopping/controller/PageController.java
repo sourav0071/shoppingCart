a6eb0103a715e95837b0576946194ddef63d7d68
sourav <sourav@gayen-PC>
2017-08-03 22:47:23 +0530

    commit after complete theme

--- onlileShopping/src/main/java/net/skg/onlileShopping/controller/PageController.java
+++ onlileShopping/src/main/java/net/skg/onlileShopping/controller/PageController.java
@@ -11,28 +11,24 @@ public class PageController {
 	@RequestMapping(value={"/", "/home", "/index"})
 	public ModelAndView index(){
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting","Welcome to Spring MVC");
+		mv.addObject("title","home");
+		mv.addObject("userClick",true);
 		return mv;
 	}
-//	@RequestMapping(value="test")
-//	public ModelAndView test(@RequestParam (value="greeting", required=false) String greeting){
-//		ModelAndView mv=new ModelAndView("page");
-//		if(null == greeting)
-//			mv.addObject("greeting","Hello there");
-//		else
-//			mv.addObject("greeting",greeting);
-//		return mv;
-//	}
 	
-	@RequestMapping(value="test/{greeting}")
-	public ModelAndView test(@PathVariable("greeting") String greeting){
+	@RequestMapping(value={"/about"})
+	public ModelAndView about(){
 		ModelAndView mv=new ModelAndView("page");
-		if(null == greeting)
-			mv.addObject("greeting","Hello there");
-		else
-			mv.addObject("greeting",greeting);
-		
-		System.out.println(greeting);
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
+		return mv;
+	}
+	
+	@RequestMapping(value={"/contact"})
+	public ModelAndView contact(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
 		return mv;
 	}
 }
